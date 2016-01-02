30946a842747a972c39866a0a8aab8daaafa408b
Chris Keathley <c@keathley.io>
2016-01-01 22:56:57 -0500

    Fix ruby installer

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 
-# We need to make sure that this is loaded otherwise we can't use chruby
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-
-if test ! $(chruby | grep 2.2.2)
+if ! [ -e ~/.rubies/ruby-2.2.2/bin/ruby ]
 then
   echo "  Installing ruby for you."
   ruby-install ruby
