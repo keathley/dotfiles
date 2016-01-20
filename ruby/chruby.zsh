aed1e68c322face556480622d63b6ab5725685d5
Chris Keathley <c@keathley.io>
2016-01-20 10:37:41 -0500

    Update chruby config

--- ruby/chruby.zsh
+++ ruby/chruby.zsh
@@ -1,2 +1,3 @@
-source /usr/local/opt/chruby/share/chruby/chruby.sh
-source /usr/local/opt/chruby/share/chruby/auto.sh
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+chruby ruby-2.2
