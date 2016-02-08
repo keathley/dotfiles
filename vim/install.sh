50670dbbbd24de1fd398bdd134dce58754530853
Chris Keathley <c@keathley.io>
2016-02-07 22:23:44 -0500

    Run vundle on initial installation

--- vim/install.sh
+++ vim/install.sh
@@ -4,4 +4,5 @@ if ! [ -e ~/.vim/bundle/Vundle.vim ]
 then
   echo "  Installing vundler for you."
   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
 fi
