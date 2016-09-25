9a27c488335a9cdbc4f945380eaf2964972e03fe
Chris Keathley <c@keathley.io>
2016-09-25 00:43:21 -0400

    Remove custom fish colors for the moment.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,33 +4,33 @@ source ~/.config/fish/aliases.fish
 
 set -e fish_greeting
 
-set -g fish_color_autosuggestion 555 yellow
-set -g fish_color_command 5f87d7
-set -g fish_color_comment 808080
-set -g fish_color_cwd 87af5f
-set -g fish_color_cwd_root 5f0000
-set -g fish_color_error 870000 --bold
-set -g fish_color_escape af5f5f
-set -g fish_color_history_current 87afd7
-set -g fish_color_host 5f87af
-set -g fish_color_match d7d7d7 --background=303030
-set -g fish_color_normal normal
-set -g fish_color_operator d7d7d7
-set -g fish_color_param 5f87af
-set -g fish_color_quote d7af5f
-set -g fish_color_redirection normal
-set -g fish_color_search_match --background=purple
-set -g fish_color_status 5f0000
-set -g fish_color_user 5f875f
-set -g fish_color_valid_path --underline
-
-set -g fish_color_dimmed 555
-set -g fish_color_separator 999
-
-set -g fish_pager_color_completion normal
-set -g fish_pager_color_description 555 yellow
-set -g fish_pager_color_prefix cyan
-set -g fish_pager_color_progress cyan
+# set -g fish_color_autosuggestion 555 yellow
+# set -g fish_color_command 5f87d7
+# set -g fish_color_comment 808080
+# set -g fish_color_cwd 87af5f
+# set -g fish_color_cwd_root 5f0000
+# set -g fish_color_error 870000 --bold
+# set -g fish_color_escape af5f5f
+# set -g fish_color_history_current 87afd7
+# set -g fish_color_host 5f87af
+# set -g fish_color_match d7d7d7 --background=303030
+# set -g fish_color_normal normal
+# set -g fish_color_operator d7d7d7
+# set -g fish_color_param 5f87af
+# set -g fish_color_quote d7af5f
+# set -g fish_color_redirection normal
+# set -g fish_color_search_match --background=purple
+# set -g fish_color_status 5f0000
+# set -g fish_color_user 5f875f
+# set -g fish_color_valid_path --underline
+#
+# set -g fish_color_dimmed 555
+# set -g fish_color_separator 999
+#
+# set -g fish_pager_color_completion normal
+# set -g fish_pager_color_description 555 yellow
+# set -g fish_pager_color_prefix cyan
+# set -g fish_pager_color_progress cyan
 
 # highlighting inside manpages and elsewhere
 set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
