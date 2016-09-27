c57c43405ad795548b1e48a724e2e5a666a40dc4
Chris Keathley <c@keathley.io>
2016-09-27 09:35:00 -0400

    Update fish path.

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -2,12 +2,7 @@ set PATH ./bin \
          ~/.bin \
          ~/bin \
          /usr/local/bin \
-         $HOME/.stack/programs/x86_64-osx/ghc-7.10.3/bin \
-         $HOME/.stack/programs/x86_64-osx \
-         $HOME/.local/bin \
-         # $HOME/.cabal/bin \
          /usr/local/heroku/bin \
          ./node_modules/.bin \
-         $HOME/.cargo/bin \
          $PATH
 
