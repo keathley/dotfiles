5403ab0312ede5bad3ea4865ec115573e8412ae9
Chris Keathley <c@keathley.io>
2017-03-06 11:25:24 -0500

    Remove unused path variable.

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -2,7 +2,6 @@ set PATH ./bin \
          ~/.bin \
          ~/bin \
          /usr/local/bin \
-         /usr/local/heroku/bin \
          ./node_modules/.bin \
          ~/.cargo/bin \
          $PATH
