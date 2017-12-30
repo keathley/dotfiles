10addda9ba6ff6901579920a55739690e45843ff
Chris Keathley <c@keathley.io>
2017-12-30 16:53:38 -0500

    Add gopath to fish paths

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -1,8 +1,11 @@
+export GOPATH=$HOME/go
+
 set PATH ./bin \
          ~/.bin \
          ~/bin \
          /usr/local/bin \
          ./node_modules/.bin \
          ~/.cargo/bin \
+         $GOPATH \
          $PATH
 
