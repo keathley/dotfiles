ba1d7c89bb895f6467e30d8fd225b955aba4dff3
Chris Keathley <c@keathley.io>
2019-05-24 15:00:57 -0400

    Add support for a private directory of fish functions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -10,6 +10,11 @@ end
 
 set fish_greeting ""
 
+# Add private functions for work related things to my functions directory
+set fish_function_path \
+  ~/.config/fish/functions/private \
+  $fish_function_path
+
 # highlighting inside manpages and elsewhere
 set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
 set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
@@ -19,7 +24,8 @@ set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
 set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
 set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
 
-export EDITOR="nvim"
+export EDITOR="vim"
 set -g fish_user_paths "/usr/local/opt/gettext/bin" $fish_user_paths
 export ERL_AFLAGS="-kernel shell_history enabled"
 export FZF_DEFAULT_OPTS='--height 30%'
+
