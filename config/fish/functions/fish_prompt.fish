e3d554fd214a4bb71191154df112377f00aa7535
Chris Keathley <c@keathley.io>
2020-10-25 12:27:05 -0400

    Remove old fish path stuff

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -3,36 +3,6 @@ function fish_prompt
   echo (prompt_char)" "
 end
 
-function version
-  if test -e mix.exs
-    set_color 62A
-    echo -n (elixir_version)
-    set_color normal
-  else if test -e package.json
-    set_color yellow
-    echo -n (node_version)
-    set_color normal
-  else if test -e Gemfile
-    set_color red
-    echo -n (node_version)
-    set_color normal
-  else
-    echo -n ""
-  end
-end
-
-function ruby_version
-  echo (asdf which ruby)
-end
-
-function node_version
-  echo (asdf which nodejs)
-end
-
-function elixir_version
-  echo (asdf which elixir)
-end
-
 function git_info
   set -l repo_info (command git rev-parse --git-dir --is-inside-git-dir --is-bare-repository --is-inside-work-tree --short HEAD ^/dev/null)
   test -n "$repo_info"; or return
