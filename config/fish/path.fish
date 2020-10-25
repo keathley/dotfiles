44cf657ea1355564cfea0738f6262a8bfce9c8f1
Chris Keathley <c@keathley.io>
2020-10-25 12:27:20 -0400

    Update fish path

--- config/fish/path.fish
+++ config/fish/path.fish
@@ -1,10 +1,11 @@
-export GOPATH=$HOME/go
+set GOPATH $HOME/go
 
 set PATH $PATH \
          ./bin \
          ~/.bin \
          ~/bin \
          /usr/local/bin \
+         /usr/local/sbin \
          ./node_modules/.bin \
          ~/.cargo/bin \
          $GOPATH \
